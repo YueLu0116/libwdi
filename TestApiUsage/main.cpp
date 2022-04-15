6262f3a3a5d6381e0e4e31ab9f133362cda8afcc
YueLu0116 <bit_drinkwater@163.com>
2022-04-15 20:35:27 +0800

    Add device driver information

--- TestApiUsage/main.cpp
+++ TestApiUsage/main.cpp
@@ -6,8 +6,8 @@
 
 const std::string DEFAULT_DIR = "tmp";
 const std::string INF_NAME = "MI_MAX_3.inf";
-const uint16_t targetVid = 0x18d1;
-const uint16_t targetPid = 0x4ee7;
+const uint16_t targetVid = 0x2717;
+const uint16_t targetPid = 0xFF60;
 
 inline bool isTarget(uint16_t vid, uint16_t pid) {
     return vid == targetVid && pid == targetPid;
@@ -22,8 +22,8 @@ int main(int argc, char* argv[]) {
     int ret = wdi_create_list(&list, &optList);
     if (ret == WDI_SUCCESS) {
         for (device = list; device != NULL; device = device->next) {
-            printf("Installing driver for USB device: \"%s\" (%04X:%04X)\n",
-                device->desc, device->vid, device->pid);
+            printf("Installing driver for [USB device: \"%s\" (%04X:%04X) driver type=%s]\n",
+                device->desc, device->vid, device->pid, device->driver);
             if (isTarget(static_cast<uint16_t>(device->vid), static_cast<uint16_t>(device->pid))) {
                 printf("Find the target device! [device=%s]", device->desc);
                 if (wdi_prepare_driver(device, DEFAULT_DIR.c_str(), INF_NAME.c_str(), &optPrepare) == WDI_SUCCESS) {
